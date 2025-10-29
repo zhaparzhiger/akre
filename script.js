933fc59917a530440debcf26175c1f6866ee615d
zhaparzhiger <keymav42@gmail.com>
2025-10-29 09:47:45 +0500

    wordfd

--- script.js
+++ script.js
@@ -1,8 +1,407 @@
-const slides = document.querySelectorAll('.page__slide');
-let index = 0;
+// Translations
+const translations = {
+  ru: {
+    collegeName: "Колледж информационных технологий",
+    navRegistration: "Регистрация",
+    navStages: "Этапы",
+    navWinners: "Итоги",
+    heroTitle: "Внутриколледжный чемпионат ВСК",
+    heroSubtitle: "Покажи свои знания и умения!",
+    formTitle: "Регистрация участника",
+    labelFullName: "Фамилия, имя, отчество",
+    labelGroup: "Группа",
+    labelSpecialty: "Специальность",
+    labelPhone: "Контактный телефон (WhatsApp)",
+    labelCompetency: "Компетенция",
+    selectSpecialty: "Выберите специальность",
+    selectCompetency: "Выберите компетенцию",
+    specialtyProgramming: "Программирование",
+    specialtyWebDev: "Веб-разработка",
+    specialtyDesign: "Дизайн",
+    specialtyNetworks: "Сетевые технологии",
+    specialtyDatabases: "Базы данных",
+    competencyWebDev: "Веб-разработка",
+    competencyProgramming: "Программирование",
+    competencyDesign: "Дизайн",
+    competencyNetworks: "Сетевые технологии",
+    btnSubmit: "Отправить заявку",
+    errorRequired: "Это поле обязательно",
+    errorPhone: "Введите корректный номер телефона",
+    successMessage: "Ваша заявка принята! Спасибо за участие.",
+    ticker1: "Регистрация до 15 февраля",
+    ticker2: "Конкурс 20–24 февраля",
+    ticker3: "Итоги 25 февраля",
+    ticker4: "Победителей ждут призы!",
+    copyright: "© Колледж информационных технологий, 2025",
+    stagesTitle: "Этапы чемпионата",
+    stage1Date: "1-15 февраля",
+    stage1Title: "Регистрация участников",
+    stage1Desc:
+      "Прием заявок от студентов колледжа. Заполнение регистрационной формы с указанием выбранной компетенции. Подтверждение участия организаторами.",
+    stage2Date: "16-19 февраля",
+    stage2Title: "Подготовка к конкурсу",
+    stage2Desc:
+      "Ознакомление участников с заданиями и критериями оценки. Проведение консультаций и тренировочных сессий. Подготовка рабочих мест и оборудования.",
+    stage3Date: "20 февраля",
+    stage3Title: "День 1: Теоретический этап",
+    stage3Desc:
+      "Тестирование теоретических знаний по выбранной компетенции. Решение задач на знание технологий, стандартов и лучших практик. Продолжительность: 3 часа.",
+    stage4Date: "21-23 февраля",
+    stage4Title: "День 2-4: Практический этап",
+    stage4Desc:
+      "Выполнение практических заданий по компетенциям. Разработка проектов, решение реальных задач. Демонстрация профессиональных навыков. По 6 часов ежедневно.",
+    stage5Date: "24 февраля",
+    stage5Title: "День 5: Защита проектов",
+    stage5Desc:
+      "Презентация выполненных работ перед жюри. Ответы на вопросы экспертов. Демонстрация функционала разработанных решений. Оценка работ по критериям.",
+    stage6Date: "25 февраля",
+    stage6Title: "Подведение итогов",
+    stage6Desc:
+      "Объявление результатов чемпионата. Награждение победителей и призеров. Вручение дипломов и призов. Торжественная церемония закрытия чемпионата.",
+    compWebDev: "Веб-разработка",
+    compProgramming: "Программирование",
+    compDesign: "Дизайн",
+    compNetworks: "Сетевые технологии",
+    winner1Name: "Иванов Алексей Петрович",
+    winner1Group: "Группа ИС-301",
+    winner2Name: "Петрова Мария Сергеевна",
+    winner2Group: "Группа ИС-302",
+    winner3Name: "Сидоров Дмитрий Иванович",
+    winner3Group: "Группа ИС-301",
+    winner4Name: "Козлов Андрей Викторович",
+    winner4Group: "Группа ПО-201",
+    winner5Name: "Новикова Елена Александровна",
+    winner5Group: "Группа ПО-202",
+    winner6Name: "Морозов Игорь Николаевич",
+    winner6Group: "Группа ПО-201",
+    winner7Name: "Волкова Анна Дмитриевна",
+    winner7Group: "Группа ДИЗ-101",
+    winner8Name: "Соколов Максим Андреевич",
+    winner8Group: "Группа ДИЗ-102",
+    winner9Name: "Лебедева Ольга Павловна",
+    winner9Group: "Группа ДИЗ-101",
+    winner10Name: "Павлов Сергей Владимирович",
+    winner10Group: "Группа СА-401",
+    winner11Name: "Федорова Татьяна Игоревна",
+    winner11Group: "Группа СА-402",
+    winner12Name: "Михайлов Артем Евгеньевич",
+    winner12Group: "Группа СА-401",
+  },
+  kz: {
+    collegeName: "Ақпараттық технологиялар колледжі",
+    navRegistration: "Тіркелу",
+    navStages: "Кезеңдер",
+    navWinners: "Қорытынды",
+    heroTitle: "Колледжішілік ВСК чемпионаты",
+    heroSubtitle: "Өз білімің мен шеберлігіңді көрсет!",
+    formTitle: "Қатысушыны тіркеу",
+    labelFullName: "Тегі, аты, әкесінің аты",
+    labelGroup: "Топ",
+    labelSpecialty: "Мамандық",
+    labelPhone: "Байланыс телефоны (WhatsApp)",
+    labelCompetency: "Құзыреттілік",
+    selectSpecialty: "Мамандықты таңдаңыз",
+    selectCompetency: "Құзыреттілікті таңдаңыз",
+    specialtyProgramming: "Программалау",
+    specialtyWebDev: "Веб-әзірлеу",
+    specialtyDesign: "Дизайн",
+    specialtyNetworks: "Желілік технологиялар",
+    specialtyDatabases: "Деректер қорлары",
+    competencyWebDev: "Веб-әзірлеу",
+    competencyProgramming: "Программалау",
+    competencyDesign: "Дизайн",
+    competencyNetworks: "Желілік технологиялар",
+    btnSubmit: "Өтінім жіберу",
+    errorRequired: "Бұл өріс міндетті",
+    errorPhone: "Дұрыс телефон нөмірін енгізіңіз",
+    successMessage: "Сіздің өтінішіңіз қабылданды! Қатысқаныңыз үшін рахмет.",
+    ticker1: "15 ақпанға дейін тіркелу",
+    ticker2: "20-24 ақпан конкурс",
+    ticker3: "25 ақпан қорытынды",
+    ticker4: "Жеңімпаздарды сыйлықтар күтеді!",
+    copyright: "© Ақпараттық технологиялар колледжі, 2025",
+    stagesTitle: "Чемпионат кезеңдері",
+    stage1Date: "1-15 ақпан",
+    stage1Title: "Қатысушыларды тіркеу",
+    stage1Desc:
+      "Колледж студенттерінен өтінімдерді қабылдау. Таңдалған құзыреттілікті көрсете отырып тіркеу нысанын толтыру. Ұйымдастырушылардың қатысуын растау.",
+    stage2Date: "16-19 ақпан",
+    stage2Title: "Конкурсқа дайындық",
+    stage2Desc:
+      "Қатысушыларды тапсырмалармен және бағалау критерийлерімен таныстыру. Кеңестер мен жаттығу сессияларын өткізу. Жұмыс орындары мен жабдықтарды дайындау.",
+    stage3Date: "20 ақпан",
+    stage3Title: "1-күн: Теориялық кезең",
+    stage3Desc:
+      "Таңдалған құзыреттілік бойынша теориялық білімді тексеру. Технологиялар, стандарттар және үздік тәжірибелерді білу бойынша тапсырмаларды шешу. Ұзақтығы: 3 сағат.",
+    stage4Date: "21-23 ақпан",
+    stage4Title: "2-4 күн: Практикалық кезең",
+    stage4Desc:
+      "Құзыреттіліктер бойынша практикалық тапсырмаларды орындау. Жобаларды әзірлеу, нақты міндеттерді шешу. Кәсіби дағдыларды көрсету. Күн сайын 6 сағаттан.",
+    stage5Date: "24 ақпан",
+    stage5Title: "5-күн: Жобаларды қорғау",
+    stage5Desc:
+      "Орындалған жұмыстарды қазылар алқасы алдында презентациялау. Сарапшылардың сұрақтарына жауап беру. Әзірленген шешімдердің функционалын көрсету. Критерийлер бойынша жұмыстарды бағалау.",
+    stage6Date: "25 ақпан",
+    stage6Title: "Қорытындыларды шығару",
+    stage6Desc:
+      "Чемпионат нәтижелерін жариялау. Жеңімпаздар мен жүлдегерлерді марапаттау. Дипломдар мен сыйлықтарды табыс ету. Чемпионатты жабудың салтанатты рәсімі.",
+    winnersTitle: "Чемпионат жеңімпаздары",
+    compWebDev: "Веб-әзірлеу",
+    compProgramming: "Программалау",
+    compDesign: "Дизайн",
+    compNetworks: "Желілік технологиялар",
+    winner1Name: "Иванов Алексей Петрович",
+    winner1Group: "ИС-301 тобы",
+    winner2Name: "Петрова Мария Сергеевна",
+    winner2Group: "ИС-302 тобы",
+    winner3Name: "Сидоров Дмитрий Иванович",
+    winner3Group: "ИС-301 тобы",
+    winner4Name: "Козлов Андрей Викторович",
+    winner4Group: "ПО-201 тобы",
+    winner5Name: "Новикова Елена Александровна",
+    winner5Group: "ПО-202 тобы",
+    winner6Name: "Морозов Игорь Николаевич",
+    winner6Group: "ПО-201 тобы",
+    winner7Name: "Волкова Анна Дмитриевна",
+    winner7Group: "ДИЗ-101 тобы",
+    winner8Name: "Соколов Максим Андреевич",
+    winner8Group: "ДИЗ-102 тобы",
+    winner9Name: "Лебедева Ольга Павловна",
+    winner9Group: "ДИЗ-101 тобы",
+    winner10Name: "Павлов Сергей Владимирович",
+    winner10Group: "СА-401 тобы",
+    winner11Name: "Федорова Татьяна Игоревна",
+    winner11Group: "СА-402 тобы",
+    winner12Name: "Михайлов Артем Евгеньевич",
+    winner12Group: "СА-401 тобы",
+  },
+  en: {
+    collegeName: "College of Information Technology",
+    navRegistration: "Registration",
+    navStages: "Stages",
+    navWinners: "Results",
+    heroTitle: "Internal College WSK Championship",
+    heroSubtitle: "Show your knowledge and skills!",
+    formTitle: "Participant Registration",
+    labelFullName: "Full Name",
+    labelGroup: "Group",
+    labelSpecialty: "Specialty",
+    labelPhone: "Contact Phone (WhatsApp)",
+    labelCompetency: "Competency",
+    selectSpecialty: "Select specialty",
+    selectCompetency: "Select competency",
+    specialtyProgramming: "Programming",
+    specialtyWebDev: "Web Development",
+    specialtyDesign: "Design",
+    specialtyNetworks: "Network Technologies",
+    specialtyDatabases: "Databases",
+    competencyWebDev: "Web Development",
+    competencyProgramming: "Programming",
+    competencyDesign: "Design",
+    competencyNetworks: "Network Technologies",
+    btnSubmit: "Submit Application",
+    errorRequired: "This field is required",
+    errorPhone: "Enter a valid phone number",
+    successMessage: "Your application has been accepted! Thank you for participating.",
+    ticker1: "Registration until February 15",
+    ticker2: "Competition February 20-24",
+    ticker3: "Results February 25",
+    ticker4: "Winners will receive prizes!",
+    copyright: "© College of Information Technology, 2025",
+    stagesTitle: "Championship Stages",
+    stage1Date: "February 1-15",
+    stage1Title: "Participant Registration",
+    stage1Desc:
+      "Accepting applications from college students. Filling out the registration form indicating the chosen competency. Confirmation of participation by organizers.",
+    stage2Date: "February 16-19",
+    stage2Title: "Competition Preparation",
+    stage2Desc:
+      "Familiarizing participants with tasks and evaluation criteria. Conducting consultations and training sessions. Preparing workplaces and equipment.",
+    stage3Date: "February 20",
+    stage3Title: "Day 1: Theoretical Stage",
+    stage3Desc:
+      "Testing theoretical knowledge in the chosen competency. Solving tasks on knowledge of technologies, standards and best practices. Duration: 3 hours.",
+    stage4Date: "February 21-23",
+    stage4Title: "Day 2-4: Practical Stage",
+    stage4Desc:
+      "Performing practical tasks in competencies. Developing projects, solving real problems. Demonstrating professional skills. 6 hours daily.",
+    stage5Date: "February 24",
+    stage5Title: "Day 5: Project Defense",
+    stage5Desc:
+      "Presenting completed work to the jury. Answering expert questions. Demonstrating the functionality of developed solutions. Evaluating work according to criteria.",
+    stage6Date: "February 25",
+    stage6Title: "Summing Up",
+    stage6Desc:
+      "Announcing championship results. Awarding winners and prize-winners. Presenting diplomas and prizes. Solemn closing ceremony of the championship.",
+    winnersTitle: "Championship Winners",
+    compWebDev: "Web Development",
+    compProgramming: "Programming",
+    compDesign: "Design",
+    compNetworks: "Network Technologies",
+    winner1Name: "Ivanov Alexey Petrovich",
+    winner1Group: "Group IS-301",
+    winner2Name: "Petrova Maria Sergeevna",
+    winner2Group: "Group IS-302",
+    winner3Name: "Sidorov Dmitry Ivanovich",
+    winner3Group: "Group IS-301",
+    winner4Name: "Kozlov Andrey Viktorovich",
+    winner4Group: "Group PO-201",
+    winner5Name: "Novikova Elena Alexandrovna",
+    winner5Group: "Group PO-202",
+    winner6Name: "Morozov Igor Nikolaevich",
+    winner6Group: "Group PO-201",
+    winner7Name: "Volkova Anna Dmitrievna",
+    winner7Group: "Group DIZ-101",
+    winner8Name: "Sokolov Maxim Andreevich",
+    winner8Group: "Group DIZ-102",
+    winner9Name: "Lebedeva Olga Pavlovna",
+    winner9Group: "Group DIZ-101",
+    winner10Name: "Pavlov Sergey Vladimirovich",
+    winner10Group: "Group SA-401",
+    winner11Name: "Fedorova Tatiana Igorevna",
+    winner11Group: "Group SA-402",
+    winner12Name: "Mikhailov Artem Evgenievich",
+    winner12Group: "Group SA-401",
+  },
+}
 
-setInterval(() => {
-  slides[index].classList.remove('active');
-  index = (index + 1) % slides.length;
-  slides[index].classList.add('active');
-}, 5000);
+// Current language
+let currentLang = "ru"
+
+// Language switching
+document.addEventListener("DOMContentLoaded", () => {
+  const langButtons = document.querySelectorAll(".lang-btn")
+
+  langButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const lang = btn.getAttribute("data-lang")
+      switchLanguage(lang)
+
+      // Update active state
+      langButtons.forEach((b) => b.classList.remove("active"))
+      btn.classList.add("active")
+    })
+  })
+
+  // Initialize with current language
+  switchLanguage(currentLang)
+})
+
+function switchLanguage(lang) {
+  currentLang = lang
+  const elements = document.querySelectorAll("[data-translate]")
+
+  elements.forEach((el) => {
+    const key = el.getAttribute("data-translate")
+    if (translations[lang] && translations[lang][key]) {
+      if (el.tagName === "INPUT" || el.tagName === "BUTTON") {
+        if (el.placeholder !== undefined) {
+          el.placeholder = translations[lang][key]
+        } else {
+          el.textContent = translations[lang][key]
+        }
+      } else {
+        el.textContent = translations[lang][key]
+      }
+    }
+  })
+}
+
+// Background slider
+if (document.querySelector(".background-slider")) {
+  const images = document.querySelectorAll(".slider-image")
+  let currentIndex = 0
+
+  setInterval(() => {
+    images[currentIndex].classList.remove("active")
+    currentIndex = (currentIndex + 1) % images.length
+    images[currentIndex].classList.add("active")
+  }, 5000)
+}
+
+// Form validation and submission
+const form = document.getElementById("registrationForm")
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    // Clear previous errors
+    document.querySelectorAll(".error-message").forEach((msg) => msg.classList.remove("show"))
+    document.querySelectorAll(".form-input, .form-select").forEach((input) => input.classList.remove("error"))
+
+    let isValid = true
+
+    // Validate full name
+    const fullName = document.getElementById("fullName")
+    if (!fullName.value.trim()) {
+      showError("fullName", "fullNameError")
+      isValid = false
+    }
+
+    // Validate group
+    const group = document.getElementById("group")
+    if (!group.value.trim()) {
+      showError("group", "groupError")
+      isValid = false
+    }
+
+    // Validate specialty
+    const specialty = document.getElementById("specialty")
+    if (!specialty.value) {
+      showError("specialty", "specialtyError")
+      isValid = false
+    }
+
+    // Validate phone
+    const phone = document.getElementById("phone")
+    const phoneRegex = /^[+]?[0-9]{10,15}$/
+    if (!phone.value.trim() || !phoneRegex.test(phone.value.replace(/[\s\-$$$$]/g, ""))) {
+      showError("phone", "phoneError")
+      isValid = false
+    }
+
+    // Validate competency
+    const competency = document.getElementById("competency")
+    if (!competency.value) {
+      showError("competency", "competencyError")
+      isValid = false
+    }
+
+    if (isValid) {
+      // Show success message
+      const successMessage = document.getElementById("successMessage")
+      successMessage.classList.add("show")
+
+      // Reset form
+      form.reset()
+
+      // Hide success message after 5 seconds
+      setTimeout(() => {
+        successMessage.classList.remove("show")
+      }, 5000)
+    }
+  })
+}
+
+function showError(inputId, errorId) {
+  const input = document.getElementById(inputId)
+  const error = document.getElementById(errorId)
+  input.classList.add("error")
+  error.classList.add("show")
+}
+
+// Phone input formatting
+const phoneInput = document.getElementById("phone")
+if (phoneInput) {
+  phoneInput.addEventListener("input", (e) => {
+    let value = e.target.value.replace(/\D/g, "")
+    if (value.length > 0) {
+      if (value[0] === "7" || value[0] === "8") {
+        value = "+7" + value.substring(1)
+      } else if (value[0] !== "+") {
+        value = "+" + value
+      }
+    }
+    e.target.value = value
+  })
+}
